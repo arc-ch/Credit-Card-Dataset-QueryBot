fd6bb2aac2055cbe1fad214afce7d63f2c930602
Archit Choudhury <134518231+arc-ch@users.noreply.github.com>
2024-06-05 13:13:25 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from pandasai import SmartDataframe
+from pandasai.responses.response_parser import ResponseParser
+from langchain_community.llms import GooglePalm
+from data import load_data
+
+class CustomResponseParser(ResponseParser):
+    def __init__(self, context) -> None:
+        super().__init__(context)
+
+    def format_dataframe(self, result):
+        st.dataframe(result["value"])
+        return
+
+    def format_plot(self, result):
+        st.image(result["value"])
+        return
+
+    def format_other(self, result):
+        st.write(result["value"])
+        return
+
+st.write("# Chat with Credit Card Fraud Dataset 🦙")
+
+df = load_data("./data")
+
+with st.expander("🔎 Dataframe Preview"):
+    st.write(df.tail(3))
+
+query = st.text_area("🗣️ Chat with Dataframe")
+container = st.container()
+
+if query:
+    # Initialize the Google PaLM API client
+    api_key = 'AIzaSyDlXYnP2xNNa0DNa7dPN89u2L4IuAchEg4'
+    llm = GooglePalm(google_api_key=api_key, temperature=0.2)
+    
+    query_engine = SmartDataframe(
+        df,
+        config={
+            "llm": llm,
+            "response_parser": CustomResponseParser,
+        }
+    )
+
+    answer = query_engine.chat(query)
+    st.write(answer)  # Display the final answer
